7d5671cae1b98de2a767bbb7148d86172974982a
u21615846 <u21615846@tuks.co.za>
2024-08-22 19:11:26 +0200

    LandingPagePt2

--- FrontEnd/Thirst6-Project/src/app/app.routes.ts
+++ FrontEnd/Thirst6-Project/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
+
+//Landing (Home) Page
 import { LandingPageComponent } from '../landing/landing-page/landing-page.component';
+import { DashboardComponent } from '../dashboard/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    {path: 'home', component: LandingPageComponent}
+    {path: 'home', component: LandingPageComponent},
+    {path:'dashboard', component: DashboardComponent}
 
 ];
---
+++ FrontEnd/Thirst6-Project/src/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
