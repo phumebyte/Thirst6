52b08a2c70baf155feb59c82f4bd62174b82c325
u21615846 <u21615846@tuks.co.za>
2024-08-22 18:49:26 +0200

    LandingPagePt1

--- FrontEnd/Thirst6-Project/src/app/app.config.ts
+++ FrontEnd/Thirst6-Project/src/app/app.config.ts
@@ -1,9 +1,27 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+//imported by Nonhlelo
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgModule } from '@angular/core';
+
+
+//debugging imports
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    provideClientHydration(),
+    BrowserAnimationsModule,
+    BrowserModule,provideAnimationsAsync(),
+    NgModule]
 };
